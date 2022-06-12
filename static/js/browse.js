ec816033533d3714e04c48f94e1396e48a7d98dc
Chengfeng Ren <chengfengren@gmail.com>
2022-06-12 00:12:50 +0000

    carousel slide works

--- static/js/browse.js
+++ static/js/browse.js
@@ -1,10 +1,16 @@
-let curentScrollPosition =0;
+let currentScrollPosition = 0;
 let scrollAmount = 320;
 
 const sCont = document.querySelector(".cuisines-container");
 const hScroll = document.querySelector(".horizontal-scroll");
 let maxScroll = -sCont.offsetWidth+ hScroll.offsetWidth;
 function scrollHonrizontally(val){
-    curentScrollPosition += (val*scrollAmount);
+    currentScrollPosition += (val*scrollAmount);
+    if (currentScrollPosition >0){
+        currentScrollPosition = 0;
+    }
+    if (currentScrollPosition<maxScroll){
+        currentScrollPosition = maxScroll;
+    }
     sCont.style.left = currentScrollPosition + 'px';
 }
