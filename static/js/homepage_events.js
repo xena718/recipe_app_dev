1cdb0aff745a61f4f49a4db5b17c48d24b773ed5
Chengfeng Ren <chengfengren@gmail.com>
2022-06-14 20:38:28 +0000

    06/14/22 version. Flask process save/remove actions

--- static/js/homepage_events.js
+++ static/js/homepage_events.js
@@ -27,27 +27,27 @@ document.querySelector('#add-to-saved-form').addEventListener('submit',evt =>{
 
 
 // from AJAX lecture demo
-'use strict';
+// 'use strict';
 
-document.querySelector('#order-form').addEventListener('submit', evt => {
-  evt.preventDefault();
+// document.querySelector('#order-form').addEventListener('submit', evt => {
+//   evt.preventDefault();
 
-  const formInputs = {
-    type: document.querySelector('#type-field').value,
-    amount: document.querySelector('#amount-field').value,
-  };
+//   const formInputs = {
+//     type: document.querySelector('#type-field').value,
+//     amount: document.querySelector('#amount-field').value,
+//   };
 
-  fetch('/new-order', {
-    method: 'POST',
-    body: JSON.stringify(formInputs),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => response.json())
-    .then(responseJson => {
-      alert(responseJson.status);
-    });
-});
+//   fetch('/new-order', {
+//     method: 'POST',
+//     body: JSON.stringify(formInputs),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then(response => response.json())
+//     .then(responseJson => {
+//       alert(responseJson.status);
+//     });
+// });
 
 
