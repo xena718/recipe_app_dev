ada871a0ec557a33d30c81bd4010c156fb1224e6
Chengfeng Ren <chengfengren@gmail.com>
2022-06-25 17:09:15 +0000

    update recipe page

--- static/js/recipe_details_events.js
+++ static/js/recipe_details_events.js
@@ -12,3 +12,39 @@ document.querySelector("#servings-size-form").addEventListener('submit',evt => {
     }
     
 });
+
+
+// below are example code
+
+const btns = document.querySelectorAll('.save-remove-btn')
+
+for (const btn of btns) {
+
+    btn.addEventListener('click',evt =>{
+    // console.log(btn)
+    const addRemoveToServer ={recipeId: btn.dataset.recipeId};
+    // console.log(dataToServer);
+
+    fetch('/save-remove',{
+        method: 'POST',
+        body: JSON.stringify(addRemoveToServer),
+        headers: {
+            'Content-Type': 'application/json',
+            },
+    })
+    .then(response => response.text())
+    .then(serverData => {
+        // const icon = btn.children[0] # this works, but only when icon is the first child.
+        const icon = btn.querySelector("i")
+        if (serverData === "just_saved"){
+            icon.classList.replace("bi-heart", "bi-heart-fill")
+        }else if (serverData === "removed_from_saved"){
+            icon.classList.replace("bi-heart-fill", "bi-heart")
+        }
+    });
+    // catch errors when server is down for example. 
+    // recipe is deleted by user A while user b is trying to save the recipe.
+
+    });
+
+}
