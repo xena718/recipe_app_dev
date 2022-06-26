626f1f330208c5c0f0534a4ef2fa1d092f68f866
Chengfeng Ren <chengfengren@gmail.com>
2022-06-26 00:08:43 +0000

    update ingredient quantity based on servering sizes

--- static/js/recipe_details_events.js
+++ static/js/recipe_details_events.js
@@ -2,31 +2,26 @@
 
 document.querySelector("#servings-size-form").addEventListener('submit',evt => {
     evt.preventDefault();
-    const servingsSize = Number(document.querySelector("#servings-size-adjust").value);
-    const servingsSizeDefault = Number(document.querySelector("#servings-size-adjust").defaultValue);
-    // console.log(servingsSize);
-    // console.log(servingsSizeDefault);
+    const servingsSize = Number(document.querySelector("#servings-size-input").value);
+    const servingsSizeDefault = Number(document.querySelector("#servings-size-input").defaultValue);
 
     if (servingsSize !== servingsSizeDefault){
-        const allIngredientCheckBoxes = document.querySelectorAll(".form-check-input")
-        // const allIngredientInputLabels = document.querySelectorAll(".form-check-label")
+        const allIngredientCheckBoxes = document.querySelectorAll(".form-check-input");
         // const ingredient = document.querySelector(".form-check-input").defaultValue;
-        // console.log(allIngredientInputLabels);
+        // ingredient is a string...
         for ( const ingredientCheckBox of allIngredientCheckBoxes){
-            const ingredient = ingredientCheckBox.defaultValue;
             const checkBoxLabel = ingredientCheckBox.nextElementSibling;
-            console.log(ingredient.name);
-            if (ingredient.quantity !==""){
-                // console.log(ingredientInputLabel.textContent)
-                // console.log(ingredient.quantity)
-                checkBoxLabel.textContent = `${servingsSize/servingsSizeDefault}`
-                // checkBoxLabel.textContent = `${ingredient.quantity}`
-                
-                // checkBoxLabel.textContent = `${servingsSize/servingsSizeDefault*(ingredient.quantity)}`
-                // checkBoxLabel.textContent = `${servingsSize/servingsSizeDefault*(ingredient.quantity)} ${ingredient.unit.name} ${ingredient.name}`
-            }else{
-                // checkBoxLabel.textContent = `${servingsSize/servingsSizeDefault}`
-                // checkBoxLabel.textContent = `${ingredient.quantity} ${ingredient.unit.name} ${ingredient.name}`
+            const ingredientQuantity = checkBoxLabel.querySelector(".ingredient-quantity");
+            if (ingredientQuantity.innerText !==""){
+                // updatedQuantity = servingsSize/servingsSizeDefault*Number(ingredientQuantity.innerText)
+                // quantityFraction = fc(updatedQuantity).toFraction()
+                // ingredientQuantity.innerText = `${quantityFraction}`
+                const updatedQuantity = servingsSize/servingsSizeDefault*eval(ingredientQuantity.innerText);
+                if (Number.isInteger(updatedQuantity)){
+                    ingredientQuantity.innerText = `${updatedQuantity}`
+                } else{
+                    ingredientQuantity.innerText = `${updatedQuantity.toFixed(1)}`;
+                }
             }
         }
     }
@@ -34,37 +29,7 @@ document.querySelector("#servings-size-form").addEventListener('submit',evt => {
 });
 
 
-// below are example code
-
-const btns = document.querySelectorAll('.save-remove-btn')
-
-for (const btn of btns) {
-
-    btn.addEventListener('click',evt =>{
-    // console.log(btn)
-    const addRemoveToServer ={recipeId: btn.dataset.recipeId};
-    // console.log(dataToServer);
-
-    fetch('/save-remove',{
-        method: 'POST',
-        body: JSON.stringify(addRemoveToServer),
-        headers: {
-            'Content-Type': 'application/json',
-            },
-    })
-    .then(response => response.text())
-    .then(serverData => {
-        // const icon = btn.children[0] # this works, but only when icon is the first child.
-        const icon = btn.querySelector("i")
-        if (serverData === "just_saved"){
-            icon.classList.replace("bi-heart", "bi-heart-fill")
-        }else if (serverData === "removed_from_saved"){
-            icon.classList.replace("bi-heart-fill", "bi-heart")
-        }
-    });
-    // catch errors when server is down for example. 
-    // recipe is deleted by user A while user b is trying to save the recipe.
-
-    });
-
-}
+// below are for handling adding ingredients to shopping list.
+document.querySelector(".add-ingredients-to-shoppinglist-btn").addEventListener('submit',evt => {
+    evt.preventDefault();
+})
